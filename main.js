1ab611347b028be23ac8e18244abc580ceb67c8f
Akash kumar <akash8825317696@gmail.com>
2023-02-28 23:57:10 +0530

    28feb2023

--- main.js
+++ main.js
@@ -447,3 +447,100 @@ this: reference to an object
 //     </div>,
 //     document.getElementById("root")
 // )
+
+//28Feb2023
+//Essential concepts of javascript
+
+//   export { getDate, year };
+// import { getDate, year as currentYear } from './utils/date.js';
+
+// export default function getDate() {
+//     const year = new Date().getFullYear();
+//     const date = `${new Date()}, year: ${year}`;  
+//     return date;
+//   }
+// import getDateNow from './utils/date.js';
+
+
+// STATE MANAGEMENT
+// state - data to be managed in app
+// state -> status
+
+// class App {
+//     constructor() {
+//       this.state = {
+//          isAuth: false,
+//          error: ''  
+//       };  
+        
+//       this.checkAuth();
+//       this.render();
+//       // this.$userMessage = document.getElementById("user-message");
+//     }
+  
+//     checkAuth() {
+//       const user = false;
+//       if (user) {
+//         this.state = { ...this.state, isAuth: true };
+//       //   this.$userMessage.textContent = "Welcome back!";
+//       } else {
+//         this.state = { ...this.state, error: "You must sign in!" };
+//       //   this.$userMessage.textContent = "You must sign in!";
+//       //   this.$userMessage.style.color = "red";
+//       }
+//     }
+  
+//     render() {
+//       const { isAuth, error } = this.state; 
+        
+//       document.getElementById("root").innerHTML = `
+//         <div style="color: ${error && 'red'}">
+//           ${isAuth ? 'Welcome back!' : error}
+//         </div>
+//       `;
+//     }
+//   }
+  
+//   new App();
+
+
+//REDUCERS
+
+//// reducers - helps us manage app state
+// (state, action) => newState;
+
+// function counterReducer(state, action) {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { ...state, count: state.count + 1 }; 
+//     case 'DECREMENT':  
+//       return { ...state, count: state.count - 1 }; 
+//     default:
+//       return state;
+//   }  
+// }
+
+// counterReducer(0, { type: 'INCREMENT' }); // 1
+// const result = counterReducer(1, { type: 'DECREMENT' }); // 0
+// console.log(result === 0);
+
+// const initialUser = {
+//     name: 'Mark',
+//     email: 'mark@gmail.com'  
+//   };
+  
+//   function userReducer(state, action) {
+//     switch (action.type) {
+//        case "CHANGE_NAME":
+//          return { ...state, name: action.payload.name };
+//        case "CHANGE_EMAIL":
+//          return { ...state, email: action.payload.email };
+//        default:
+//          return state; 
+//     }  
+//   }
+  
+//   const result = userReducer(initialUser, { type: 'CHANGE_EMAIL', payload: { email: 'mark@compuserve.com' } });
+//   console.log(result.email === 'mark@compuserve.com');
+  // action.payload.name;
+  // { type: 'CHANGE_EMAIL', payload: { email: 'joe@gmail.com' } }
