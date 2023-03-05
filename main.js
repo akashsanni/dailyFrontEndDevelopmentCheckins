153635888126829f380ca66c963a02b614dc9073
Akash kumar <akash8825317696@gmail.com>
2023-03-05 23:55:15 +0530

    5Mar2023

--- main.js
+++ main.js
@@ -563,3 +563,65 @@ this: reference to an object
 // ›Doug,0,["Doug", "Fred", "Jane"],undefined
 // ›Fred,1,["Doug", "Fred", "Jane"],undefined
 // ›Jane,2,["Doug", "Fred", "Jane"],undefined
+
+
+// 1. Why do we need to `import React from "react"` in our files?
+// React is what defines JSX
+
+// 2. If I were to console.log(page) in index.js, what would show up?
+// A JavaScript object. React elements that describe what React should
+// eventually add to the real DOM for us.
+
+// 3. What's wrong with this code:
+// ```
+// const page = (
+//     <h1>Hello</h1>
+//     <p>This is my website!</p>
+// )
+// ```
+// We need our JSX to be nested under a single parent element
+
+// 4. What does it mean for something to be "declarative" instead of "imperative"?
+// Declarative means I can tell the computer WHAT to do 
+// and expect it to handle the details. Imperative means I need
+// to tell it HOW to do each step.
+
+// 5. What does it mean for something to be "composable"?
+// We have small pieces that we can put together to make something
+// larger/greater than the individual pieces.
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// const renderElement = (
+//     <div className="challenge-content">
+//         <img src="./react-logo.png" width="40px"/>
+//         <h1>Fun facts about React</h1>
+//         <ul>
+//         <li>Was first released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 100k stars on Github</li>
+//         <li>Is maintained by facebook</li>
+//         <li>Powers of thousands of enterprise app,including mobile apps</li>
+//         </ul>
+//     </div>
+// )
+// ReactDOM.render(renderElement,document.getElementById("root"))
+
+//import React from "react"
+// import ReactDOM from "react-dom"
+
+// const renderElement = (
+//     <div className="challenge-content">
+//         <img src="./react-logo.png" width="40px"/>
+//         <h1>Fun facts about React</h1>
+//         <ul>
+//         <li>Was first released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 100k stars on Github</li>
+//         <li>Is maintained by facebook</li>
+//         <li>Powers of thousands of enterprise app,including mobile apps</li>
+//         </ul>
+//     </div>
+// )
+// ReactDOM.render(renderElement,document.getElementById("root"))
